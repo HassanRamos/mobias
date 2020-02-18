d38ae26458260a0383294a3828ae95e93592d9b0
HassanRamos <ramoshassan42@gmail.com>
2020-02-18 15:12:54 +0300

    added files

--- app.js
+++ app.js
@@ -3,6 +3,73 @@ var  bodyParser  =  require ( 'body-parser' ) ;
 var  request  =  require ( 'request' ) ;
 var  app  =  express ( ) ;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var path = require("path");
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+//***************Webhook*************** *//
+
+app.post('/webhook',function(req,res){
+    console.log('Received a post request');
+    if(!req.body)return res.sendStatus(400);
+    res.setHeader('Content-Type','application/json');
+    console.log('here is the post request from DialogFlow');
+    console.log(req.body);
+    console.log('Got geo city parameter from DialogFlow'+req.body.queryResult.parameters['geo-city']);
+        var city = req.body.queryResult.parameters['geo-city'];
+        var w = getWeather(city);
+        let response = "";//Default response from the webhook to show its working
+        let responseObj = {
+            "fulfillmentText":response,
+            "fulfillmentMessages":[{"text":[w]}],
+            "source":""
+        }
+    console.log('Here is the response to dialogFlow');
+    console.log(responseObj);
+    return res.json(responseObj);
+    //res.send(JSON.stringfy({speech:w,displayText:w,source:'api ai-weather-webhook}));*/
+})
+
+//********Weather API****** *//
+var apiKey = '303bfc17dc8102d581d1c1dbb10eb857';
+var result;
+
+function cb(err,response,body){
+    if(err){
+        console.log('error',error);
+
+    }
+    var weather = JSON.parse(body)
+    if(weather.message === 'city not found'){
+        result = 'Unable to get weather'+weather.message;
+    }
+    else{
+        result = 'Right now its'+weather.main.temp+'degrees with'+weather.weather[0].description;
+    }
+}
+function getWeather(city){
+    result = undefined;
+    var url = `http://samples.openweathermap.org/data/2.5/weather?q={city},ke&appid={apiKey}`;
+    console.log(url);
+    var req = request(url,cb);
+    while(result === undefined){
+        require('deasync').runLoopOnce();
+    }
+    return result;
+    
+}
+
+
+
+
+
+/*var  express  =  require ( 'express' ) ;
+var  bodyParser  =  require ( 'body-parser' ) ;
+var  request  =  require ( 'request' ) ;
+var  app  =  express ( ) ;
+
 // The body of requests received will be formatted in JSON format
 app . use ( bodyParser . urlencoded ( {  extended : false  } ) ) ;
 app . use ( bodyParser . json ( ) ) ;
@@ -39,7 +106,7 @@ app . post ( '/ weather' ,  function  ( req ,  res )  {
       arrangement has a length of zero, it means that the city was not found, you have to take
       In the case of Spanish, the tildes or some special character may not generate
       found the searched city */
-      if ( body.length  ==  0 ) {
+     /* if ( body.length  ==  0 ) {
         console.log ( 'City not found' ) ;
         climate . fulfillmentText  =  'Your city was not found, make sure you wrote it correctly' ;
         res . json ( climate ) ;
---
+++ index.js
@@ -0,0 +1,62 @@
+var  express  =  require ( 'express' ) ;
+var  bodyParser  =  require ( 'body-parser' ) ;
+var  request  =  require ( 'request' ) ;
+var  app  =  express ( ) ;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var path = require("path");
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+//***************Webhook*************** *//
+
+app.post('/webhook',function(req,res){
+    console.log('Received a post request');
+    if(!req.body)return res.sendStatus(400);
+    res.setHeader('Content-Type','application/json');
+    console.log('here is the post request from DialogFlow');
+    console.log(req.body);
+    console.log('Got geo city parameter from DialogFlow'+req.body.queryResult.parameters['geo-city']);
+        var city = req.body.queryResult.parameters['geo-city'];
+        var w = getWeather(city);
+        let response = "";//Default response from the webhook to show its working
+        let responseObj = {
+            "fulfillmentText":response,
+            "fulfillmentMessages":[{"text":[w]}],
+            "source":""
+        }
+    console.log('Here is the response to dialogFlow');
+    console.log(responseObj);
+    return res.json(responseObj);
+    //res.send(JSON.stringfy({speech:w,displayText:w,source:'api ai-weather-webhook}));*/
+})
+
+//********Weather API****** *//
+var apiKey = '303bfc17dc8102d581d1c1dbb10eb857';
+var result;
+
+function cb(err,response,body){
+    if(err){
+        console.log('error',error);
+
+    }
+    var weather = JSON.parse(body)
+    if(weather.message === 'city not found'){
+        result = 'Unable to get weather'+weather.message;
+    }
+    else{
+        result = 'Right now its'+weather.main.temp+'degrees with'+weather.weather[0].description;
+    }
+}
+function getWeather(city){
+    result = undefined;
+    var url = `http://samples.openweathermap.org/data/2.5/weather?q={city},ke&appid={apiKey}`;
+    console.log(url);
+    var req = request(url,cb);
+    while(result === undefined){
+        require('deasync').runLoopOnce();
+    }
+    return result;
+    
+}
